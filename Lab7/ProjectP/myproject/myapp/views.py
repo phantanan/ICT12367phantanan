006d665788f494ea1bdc7b02ff186221309c73e2
phantanan <109572154+phantanan@users.noreply.github.com>
2026-02-23 14:37:08 +0700

    Add files via upload

---
+++ Lab7/ProjectP/myproject/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>ICT12367 SPU</h1>")
+
+def about(request):
+    return HttpResponse("<h1>เกี่ยวกับเรา</h1>")
+
+def form(request):
+    return render(request, 'form.html')
+
+def contact(request):
+    return HttpResponse("<h1>68061587 พันธนันท์ เกลื่อนการ</h1>")
